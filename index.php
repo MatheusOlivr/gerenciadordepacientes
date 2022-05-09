28b1ba20235575f8542e81ec7f1f37ffaf0e4b7e
MatheusOlivr <contato.matheusoliv@gmail.com>
2022-05-09 19:29:25 -0300

    Carrega as imagens que serão utilizadas no projeto e cria o arquivo de estilo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "shortcut icon" href = "src/images/favicon.ico"/>
+    <link rel = "stylesheet" href = "src/styles/main.css"/>
+    <title>Pagina inicial</title>
+</head>
+<body>
+    
+</body>
+</html>
