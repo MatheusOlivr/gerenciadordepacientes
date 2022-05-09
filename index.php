65e3f248fc1f4e039e61d69d79a14092bd8cebcf
MatheusOlivr <contato.matheusoliv@gmail.com>
2022-05-09 20:34:54 -0300

    Cria a pagina inicial do projeto e a estiliza

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
     <title>Pagina inicial</title>
 </head>
 <body>
-    
+    <div id = "conteudo">
+        <div class = "conteudo-bloco">
+            <div class = "conteudo-bloco-img">
+                <img src = "src/images/profissionais.jpg" alt = "enfermeiros">
+            </div>
+            <div class = "conteudo-bloco-button">
+                <h1>Pagina Inicial</h1>
+                <a href = "src/frontEnd/login.php"><button>Fazer Login</button></a>
+                <a href = "src/frontEnd/cadastrar.php"><button>Cadastrar</button></a>
+                <?php require("src/frontEnd/footer.php")?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
