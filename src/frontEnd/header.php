ed591ca9faf9b7f6b828cc28f8c7e3075f15b9cc
MatheusOlivr <contato.matheusoliv@gmail.com>
2022-05-13 20:12:29 -0300

    adiciona ao arquivo list a area de botões

--- src/frontEnd/header.php
+++ src/frontEnd/header.php
@@ -8,6 +8,6 @@
     <link rel = "stylesheet" href = <?php echo $dirCss ?>/>
     <title><?php echo $tituloPage?></title>
 </head>
-<body>
+<body id = "body">
     <div id = "conteudo">
         <div class = "conteudo-bloco">
--- src/frontEnd/list.php
+++ src/frontEnd/list.php
@@ -1,9 +1,13 @@
 <?php
     $tituloPage = "Area de login - total Care";
     $dirIcon = "../images/favicon.ico";
-    $dirCss = "../styles/main.css";
+    $dirCss = "../styles/list.css";
     require("header.php");
 ?>
+    <div class = "conteudo-bloco-links">
+        <a href = "registerPacient.php"><h1>Cadastar Paciente</h1></a>
+    <a href = "logout.php"><h1>Deslogar</h1></a>
+    </div>
     <iframe src="../backEnd/functionList.php" frameborder="0"></iframe>
 <?php
     require("footer.php");
